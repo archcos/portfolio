2bab51163909b897b86ee1c1e6da9d13ecf3ff4b
archcos <arjay.charcos25@gmail.com>
2025-04-17 23:30:44 +0800

    Test

---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+function Header() {
+    return (
+        <>
+        <div className="header">
+            <h1> Logo </h1>
+            <ul>
+                <li>
+                    <a href="#"> Home </a>
+                </li>
+                <li>
+                    <a href="#"> About Me </a>
+                </li>
+                <li>
+                    <a href="#"> Projects </a>
+                </li>
+            </ul>
+        </div>
+        </>
+    );
+}
+
+export default Header;
