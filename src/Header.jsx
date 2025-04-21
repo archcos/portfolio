a7fb569a228f39c1644bbf9728a890969b45b855
archcos <arjay.charcos25@gmail.com>
2025-04-21 18:26:01 +0800

    added logo

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,8 +1,10 @@
+import Logo from "./assets/aclogo.svg";
+
 function Header() {
     return (
         <>
-        <div className="mt-5 flex justify-between items-center sticky top-0  bg-white z-50 shadow">
-            <h1 className="ml-10  hover:bg-gray-500"><a href="#"> Logo </a></h1>
+        <div className="flex justify-between items-center sticky top-0  p-2 bg-white z-50 shadow">
+            <a href="#"><img src={Logo} alt="logo" className="ml-10 w-10 h-7"></img></a>
             <ul className="text-xs md:text-base mr-10 flex gap-4 font-sans md:font-Arial">
                 <li className="hover:bg-gray-800 hover:text-white hover:px-6 transition-all duration-300">
                     <a href="#"> HOME </a>
